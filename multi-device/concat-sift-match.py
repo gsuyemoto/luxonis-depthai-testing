306f895c238c76c762c078b0a5b0b26e0bc9d941
gsuyemoto <gsuyemoto@gmail.com>
2024-05-09 22:11:25 -0700

    Add ability to swap places of image in concatenated result.

--- multi-device/concat-sift-match.py
+++ multi-device/concat-sift-match.py
@@ -17,19 +17,22 @@ for dev in all_devices:
 pipeline1 = dai.Pipeline()
 pipeline1.setOpenVINOVersion(dai.OpenVINO.VERSION_2021_4)
 
-cam1 = pipeline1.create(dai.node.ColorCamera)
+# cam1 = pipeline1.create(dai.node.ColorCamera)
+cam1 = pipeline1.create(dai.node.Camera)
 cam1.setPreviewSize(WIDTH, HEIGHT)
-cam1.setInterleaved(False)
-cam1.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+# cam1.setInterleaved(False)
+# cam1.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
 cam1.setImageOrientation(dai.CameraImageOrientation.ROTATE_180_DEG)
 
 xout1 = pipeline1.create(dai.node.XLinkOut)
 xout1.setStreamName("device1_output_to_host")
 
 cam1.preview.link(xout1.input)
-# cam1.isp.link(xout1.input)
+# cam1.video.link(xout1.input)
 
-info1 = dai.DeviceInfo("18443010D13E411300") # MXID
+info1 = dai.DeviceInfo("18443010915D2D1300") # MXID
+# info1 = dai.DeviceInfo("18443010D13E411300") # MXID
+
 device1 = dai.Device(pipeline1, info1)
 qDevice1 = device1.getOutputQueue(name="device1_output_to_host", maxSize=4, blocking=False)
 
@@ -37,36 +40,39 @@ qDevice1 = device1.getOutputQueue(name="device1_output_to_host", maxSize=4, bloc
 pipeline2 = dai.Pipeline()
 pipeline2.setOpenVINOVersion(dai.OpenVINO.VERSION_2021_4)
 
-cam2 = pipeline2.create(dai.node.ColorCamera)
+# cam2 = pipeline2.create(dai.node.ColorCamera)
+cam2 = pipeline2.create(dai.node.Camera)
 cam2.setPreviewSize(WIDTH, HEIGHT)
-cam2.setInterleaved(False)
-cam2.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+# cam2.setInterleaved(False)
+# cam2.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
 cam2.setImageOrientation(dai.CameraImageOrientation.ROTATE_180_DEG)
 
 xout2 = pipeline2.create(dai.node.XLinkOut)
 xout2.setStreamName("device2_output_to_host")
 
 cam2.preview.link(xout2.input)
-# cam2.isp.link(xout2.input)
+# cam2.video.link(xout2.input)
 
-info2 = dai.DeviceInfo("18443010915D2D1300") # MXID
+info2 = dai.DeviceInfo("18443010D13E411300") # MXID
+# info2 = dai.DeviceInfo("18443010915D2D1300") # MXID
+
 device2 = dai.Device(pipeline2, info2)
 qDevice2 = device2.getOutputQueue(name="device2_output_to_host", maxSize=4, blocking=False)
 
-# Get first images to compute sift/homography
-img_cv1 = qDevice1.get().getCvFrame()
-img_cv2 = qDevice2.get().getCvFrame()
-
-stitcher = Stitcher([img_cv1, img_cv2])
-
+swap = False
+stitcher = None
 translateX = 0
 translateY = 0
 
 # /////////////////////// Get images from devices and stitch them together
 while True:
     # Load the images
-    img_cv1 = qDevice1.get().getCvFrame()
-    img_cv2 = qDevice2.get().getCvFrame()
+    if swap is False:
+        img1 = qDevice1.get().getCvFrame()
+        img2 = qDevice2.get().getCvFrame()
+    else:
+        img1 = qDevice2.get().getCvFrame()
+        img2 = qDevice1.get().getCvFrame()
 
     M = np.float32([
     [1, 0, translateX],
@@ -74,13 +80,18 @@ while True:
     ])
 
     if translateX != 0 or translateY != 0:
-        img_cv2 = cv2.warpAffine(img_cv2, M, (WIDTH, HEIGHT))
+        img2 = cv2.warpAffine(img2, M, (WIDTH, HEIGHT))
     
-    result = stitcher.warp([img_cv1, img_cv2])
-    if result is not None:
+    if stitcher is not None:
+        result = stitcher.warp([img1, img2])
         # print(f"Shape: {result.shape}")
-        cv2.imshow("Concatenated images", result)
+    else:
+        result = np.concatenate((img1, img2), axis=1)        
     
+    # Display concatenated image
+    cv2.imshow("Concatenated images", result)
+    
+    # Key controls
     key = cv2.waitKey(1)
     if key == ord('q'):
         print("Quitting...")
@@ -88,7 +99,7 @@ while True:
         device2.close()
         break
     elif key == ord('c'):
-        stitcher = Stitcher([img_cv1, img_cv2])
+        stitcher = Stitcher([img1, img2])
     elif key == ord(']'):
         print("Translating to right...")
         translateX += 1
@@ -101,3 +112,12 @@ while True:
     elif key == ord('d'):
         print("Translating to down...")
         translateY += 1
+    elif key == ord('r'):
+        print("Reset...")
+        stitcher = None
+    elif key == ord('s'):
+        print("Swap toggle...")
+        if swap:
+            swap = False
+        else:
+            swap = True
