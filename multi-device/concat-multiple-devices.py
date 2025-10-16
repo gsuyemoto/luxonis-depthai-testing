b708c5b4fc78fa872cb5e7923c2dc6d9e1442e39
gsuyemoto <gsuyemoto@gmail.com>
2025-10-16 00:57:06 -0700

    fix: updated concat-multiple-devices.py to Depthai V3

--- multi-device/concat-multiple-devices.py
+++ multi-device/concat-multiple-devices.py
@@ -5,40 +5,27 @@ import depthai as dai
 import contextlib
 import numpy as np
 
-def createPipeline():
-    # Start defining a pipeline
-    pipeline = dai.Pipeline()
-    # Define a source - color camera
-    camRgb = pipeline.create(dai.node.ColorCamera)
-
-    camRgb.setPreviewSize(300, 300)
-    camRgb.setBoardSocket(dai.CameraBoardSocket.CAM_A)
-    camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-    camRgb.setInterleaved(False)
-
-    # Create output
-    xoutRgb = pipeline.create(dai.node.XLinkOut)
-    xoutRgb.setStreamName("rgb")
-    camRgb.preview.link(xoutRgb.input)
-
-    return pipeline
-
+def createPipeline(pipeline):
+    camRgb = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_A)
+    output = camRgb.requestOutput((1920,1080), dai.ImgFrame.Type.NV12 ,dai.ImgResizeMode.CROP, 20).createOutputQueue()
+    return pipeline, output
 
 with contextlib.ExitStack() as stack:
     deviceInfos = dai.Device.getAllAvailableDevices()
     usbSpeed = dai.UsbSpeed.SUPER
     openVinoVersion = dai.OpenVINO.Version.VERSION_2021_4
 
-    qRgbMap = []
-    devices = []
+    print(f"Number devices available: {len(deviceInfos)}")
+
     mxids = []
+    queues = []
+    pipelines = []
 
     for deviceInfo in deviceInfos:
-        deviceInfo: dai.DeviceInfo
-        device: dai.Device = stack.enter_context(dai.Device(openVinoVersion, deviceInfo, usbSpeed))
-        devices.append(device)
-        print("===Connected to ", deviceInfo.getMxId())
-        mxId = device.getMxId()
+        pipeline = stack.enter_context(dai.Pipeline())
+        device = pipeline.getDefaultDevice()
+        print("===Connected to ", deviceInfo.deviceId)
+        mxId = device.getDeviceId()
         mxids.append(mxId)
         cameras = device.getConnectedCameras()
         usbSpeed = device.getUsbSpeed()
@@ -51,20 +38,34 @@ with contextlib.ExitStack() as stack:
         if eepromData.productName != "":
             print("   >>> Product name:", eepromData.productName)
 
-        pipeline = createPipeline()
-        device.startPipeline(pipeline)
+        # Define source and output
+        pipeline, output = createPipeline(pipeline)
+        pipeline.start()
+        pipelines.append(pipeline)
 
-        # Output queue will be used to get the rgb frames from the output defined above
-        q_rgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
-        qRgbMap.append((q_rgb, mxId))
+        queues.append(output)
 
     while True:
-        frames = [None, None]
-        for q_rgb, stream_name in qRgbMap:
-            if stream_name == mxids[0]:
-                frames[1] = q_rgb.get().getCvFrame()
-            else:
-                frames[0] = q_rgb.get().getCvFrame()
+        frames = []
+        for i, stream in enumerate(queues):
+            videoIn = stream.get()
+            assert isinstance(videoIn, dai.ImgFrame)
+            height = videoIn.getHeight()
+            width = videoIn.getWidth()
+            frame = videoIn.getData().reshape((height * 3 // 2, width)).astype(np.uint8)
+            # Convert NV12 to BGR for display and concatenation
+            frame_bgr = cv2.cvtColor(frame, cv2.COLOR_YUV2BGR_NV12)
+            # Resize frame to a common height (e.g., 360p) for display, maintaining aspect ratio
+            display_height = 360
+            display_width = int(frame_bgr.shape[1] * display_height / frame_bgr.shape[0])
+            frame_resized = cv2.resize(frame_bgr, (display_width, display_height))
+            frames.append(frame_resized)
+
+        # for q_rgb, stream_name in qRgbMap:
+        #     if stream_name == mxids[0]:
+        #         frames[1] = q_rgb.get().getCvFrame()
+        #     else:
+        #         frames[0] = q_rgb.get().getCvFrame()
         
         if len(frames) > 0:
             # Concatenate frames horizontally
@@ -75,3 +76,11 @@ with contextlib.ExitStack() as stack:
 
         if cv2.waitKey(1) == ord('q'):
             break
+
+    # while True:
+    #         for i, stream in enumerate(queues):
+    #             videoIn = stream.get()
+    #             assert isinstance(videoIn, dai.ImgFrame)
+    #             cv2.imshow(f"video_device{i}", videoIn.getCvFrame())
+    #         if cv2.waitKey(1) == ord('q'):
+    #             break
